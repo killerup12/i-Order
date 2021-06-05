731aec0c2cf1ca65af0b0170671a56ef89a69107
killerup12 <57999344+killerup12@users.noreply.github.com>
2021-06-05 14:23:39 +0300

    Init commit

---
+++ android/app/src/main/kotlin/com/shit_app_production/i_order/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.shit_app_production.i_order
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(IOrder());
+}
+
+class IOrder extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implemen
+  }
+}
